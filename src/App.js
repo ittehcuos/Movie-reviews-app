1d5187c20a6b93e288cfdf9faa743e080fb3eef9
Anto <61883433+ittehcuos@users.noreply.github.com>
2022-02-21 15:14:47 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+export default App;
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Details from './pages/Details';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar/Navbar';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<NotFound/>}/>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/details/:id" element={<Details/>}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar  />
+        <Routes>
+               <Route path='/' exact element={<Home/>} />
+               <Route path='/details/:id'  exact element={<Details/>} />
+               <Route path='/login' exact element={<Login/>} />
+               <Route patch='*' exact element={<NotFound/>} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
